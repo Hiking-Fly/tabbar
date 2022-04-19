74402655a69697cf92e89487b50b2b990532b0b6
张小飞 <2624970078@qq.com>
2022-04-19 14:20:53 +0800

    常量改写函数名和引用，响应式添加和删除

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
     <hello-vuex/>
     <button @click="add">+</button>
     <button @click="sub">-</button>
+    <button @click="add5(5)">+5</button>
     <h3>直接获取store.state的counter</h3>
     {{$store.state.counter}}
     <main-tab-bar></main-tab-bar>
@@ -14,6 +15,7 @@
 <script>
 import MainTabBar from "components/MainTabBar.vue"
 import HelloVuex from "views/HelloVuex"
+import { INCREASE } from '@/store/mutations-types.js'
 export default{
   name:"App",
   components: {
@@ -22,10 +24,18 @@ export default{
   },
   methods:{
     add(){
-      this.$store.commit("increase")
+      this.$store.commit(INCREASE)
     },
     sub(){
       this.$store.commit("decrease")
+      
+    },
+    add5(count){
+      this.$store.commit("increase5",count)
+      // this.$store.commit({
+      //   type:"increase5",
+      //   count,
+      // }) //另一种提交方式，提交过去的参数会是对象
     }
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { INCREASE } from '@/store/mutations-types.js'
 
 Vue.use(Vuex)
 
@@ -8,18 +9,28 @@ const store = new Vuex.Store({
         counter:100,
     },
     mutations:{
-        increase(state){
+        [INCREASE](state){
             state.counter++
         },
         decrease(state){
             state.counter--
+        },
+        increase5(state,count){
+            state.counter += count
+        },
+        updateInfo(state){
+            state.info['address'] = '洛杉矶'  //添加了属性，但是不是响应式的，MV VM
+            Vue.set(state.info,'address','洛杉矶') //添加属性，是响应式的
+            Vue.delete(state.info,age)//响应式删除
         }
     },
     actions:{
 
     },
-    getters:{
-
+    getters:{//类似计算属性
+        powCounter(state){
+            return state.counter * state.counter
+        }
     },
     modules:{
 
